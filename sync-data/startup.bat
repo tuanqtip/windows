4e68e5b769aa07dbcf13fd7bca466a2d82d40607
tuanqtip <tuanqtip@gmail.com>
2024-09-25 16:43:12 +0700

    Update startup.bat

--- sync-data/startup.bat
+++ sync-data/startup.bat
@@ -1,2 +1,2 @@
-﻿@echo off
-start /min powershell -ExecutionPolicy Bypass -File ".\dong-bo-1-chieu-chay-ngam.ps1"
+@echo off
+start /min powershell -ExecutionPolicy Bypass -File ".\dong-bo-1-chieu-multi-source-and-destination-chay-ngam.ps1"
